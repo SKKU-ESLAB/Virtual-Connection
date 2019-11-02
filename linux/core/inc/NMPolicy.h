1cd16152482224441ef35fe0a36d94a06d2abf2a
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-11-02 18:48:31 +0900

    Now modifying NMPolicy

--- linux/core/inc/NMPolicy.h
+++ linux/core/inc/NMPolicy.h
@@ -70,6 +70,7 @@ protected:
 
 private:
   void print_custom_event(std::string &event_string) {
+    // TODO: event_string = app_name + event_type
     // Update recent custom event timestamp
     gettimeofday(&this->mRecentCustomEventTS, NULL);
     if (!this->mIsFirstCustomEvent) {
@@ -128,6 +129,8 @@ protected:
   bool mIsFirstCustomEvent;
   struct timeval mFirstCustomEventTS;
   struct timeval mRecentCustomEventTS;
+  struct timeval mRecentAppChangeTS;
+  std::string mPresentAppName;
 }; /* class NMPolicy */
 
 } /* namespace sc */
