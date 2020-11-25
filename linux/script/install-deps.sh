6103a21ffc5deea45ae3f913cd2d732c5364cf5d
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-11-25 22:48:42 +0900

    Add script to install dependent packages (#16)

--- linux/script/install-deps.sh
+++ linux/script/install-deps.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt-get install libssl-dev libnl-3-dev libnl-genl-3-dev
+sudo apt-get update
+sudo apt-get install libssl-dev libnl-3-dev libnl-genl-3-dev -y
+sudo apt-get install libbluetooth-dev -y
+
+sudo apt-get install udhcpd -y
+sudo touch /var/lib/misc/udhcpd.leases
+sudo wpa_supplicant -iwlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
